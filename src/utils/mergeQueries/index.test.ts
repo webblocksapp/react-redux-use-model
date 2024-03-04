80db1261121acd4318fff0e7a20b37bf0669b1d4
webblocksapp <mauriver21@gmail.com>
2024-03-04 15:24:07 -0500

    More tests to mergeQueries

--- src/utils/mergeQueries/index.test.ts
+++ src/utils/mergeQueries/index.test.ts
@@ -35,6 +35,28 @@ describe('mergeQueries', () => {
     ]);
   });
 
+  it('Return existing queries when queryKey is undefined.', () => {
+    const result = mergeQueries(
+      [
+        {
+          queryKey: 'QueryKey',
+          ids: ['1', '2'],
+          queryData: undefined,
+        },
+      ],
+      undefined,
+      ['3', '4']
+    );
+
+    expect(result).toMatchObject([
+      {
+        queryKey: 'QueryKey',
+        ids: ['1', '2'],
+        queryData: undefined,
+      },
+    ]);
+  });
+
   it('Merge query with existing queries and pagination.', () => {
     const result = mergeQueries(
       [
@@ -85,6 +107,61 @@ describe('mergeQueries', () => {
         queryData: {
           pagination: { page: 1, size: 2, totalElements: 5, totalPages: 3 },
         },
+      },
+    ]);
+  });
+
+  it('Merge query with page override.', () => {
+    const result = mergeQueries(
+      [
+        {
+          queryKey: 'QueryKey',
+          ids: ['1', '2'],
+          queryData: {
+            pagination: { page: 0, size: 2, totalElements: 6, totalPages: 3 },
+          },
+        },
+      ],
+      'QueryKey',
+      ['3', '4'],
+      { pagination: { page: 2, size: 2, totalElements: 6, totalPages: 3 } },
+      1 // <-- Page override.
+    );
+
+    expect(result).toMatchObject([
+      {
+        queryKey: 'QueryKey',
+        ids: ['1', '2', undefined, undefined, '3', '4'],
+        queryData: {
+          pagination: { page: 1, size: 2, totalElements: 6, totalPages: 3 },
+        },
+      },
+    ]);
+  });
+
+  it('Adds a new query.', () => {
+    const result = mergeQueries(
+      [
+        {
+          queryKey: 'QueryKey1',
+          ids: ['1', '2'],
+          queryData: undefined,
+        },
+      ],
+      'QueryKey2',
+      ['1', '2']
+    );
+
+    expect(result).toMatchObject([
+      {
+        queryKey: 'QueryKey1',
+        ids: ['1', '2'],
+        queryData: undefined,
+      },
+      {
+        queryKey: 'QueryKey2',
+        ids: ['1', '2'],
+        queryData: undefined,
       },
     ]);
   });
