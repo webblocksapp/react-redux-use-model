300f99cb8537f1ba3178071559e7349a98a70471
webblocksapp <mauriver21@gmail.com>
2024-03-21 23:36:56 -0500

    Removed query handlers as required generic type.

--- src/examples/models/useProductModel/index.ts
+++ src/examples/models/useProductModel/index.ts
@@ -2,11 +2,10 @@ import { EntityName } from '@examples/constants';
 import { useProductApiClient } from '@examples/apiClients';
 import { EntityActionType, useModel } from 'react-redux-use-model';
 import { Product } from '@examples/interfaces';
-import { QueryHandlers } from '@interfaces';
 
 export const useProductModel = () => {
   const productApiClient = useProductApiClient();
-  const model = useModel<Product, QueryHandlers<Product>>({
+  const model = useModel<Product>({
     entityName: EntityName.Products,
     config: {
       paginationSizeMultiplier: 5,
