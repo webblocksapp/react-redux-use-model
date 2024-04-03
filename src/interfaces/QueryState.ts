813c625a8f7d650e748e60695f358e7dc2424958
webblocksapp <mauriver21@gmail.com>
2024-04-03 18:27:27 -0500

    Fix QueryState type.

--- src/interfaces/QueryState.ts
+++ src/interfaces/QueryState.ts
@@ -3,7 +3,7 @@ import { Entity, Pagination } from '@interfaces';
 export type QueryState<TEntity extends Entity = Entity, TParams = any> = {
   ids: Array<Exclude<TEntity['id'], undefined>>;
   queryKey: string;
-  timestamp: number;
+  timestamp?: number;
   params?: TParams;
   pagination?: Pagination;
   calculatedPagination?: Pagination;
