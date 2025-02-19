4e357ac4bfe34e5b4bbfb0529bc066bb64d12737
Mauricio Rivera <mauriver21@gmail.com>
2025-02-19 11:21:54 -0500

    Configure vite

--- packages/www/vite.config.ts
+++ packages/www/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
+import dts from 'vite-plugin-dts';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        tsconfigPath: './tsconfig.app.json', // Specify your custom tsconfig path
+      },
+      overlay: false,
+    }),
+    tsconfigPaths(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json',
+      include: 'src',
+    }),
+  ],
+});
