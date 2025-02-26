bbb290f7522eb2f117268c0d9b17dbd64d1d81f0
webblocksapp <mauriver21@gmail.com>
2025-02-25 20:57:33 -0500

    Refactor mergeUniqueIds using Set

--- packages/lib/src/utils/mergeUniqueIds/index.ts
+++ packages/lib/src/utils/mergeUniqueIds/index.ts
@@ -2,13 +2,5 @@ export const mergeUniqueIds = (
   ids1: Array<string | number>,
   ids2: Array<string | number>
 ) => {
-  const allIds = [...ids1, ...ids2];
-  let uniqueIds: Array<string | number> = [];
-
-  for (let id of allIds) {
-    if (uniqueIds.includes(id)) continue;
-    uniqueIds = [...uniqueIds, id];
-  }
-
-  return uniqueIds;
+  return Array.from(new Set([...ids1, ...ids2]));
 };
